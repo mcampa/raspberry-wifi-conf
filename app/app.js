1171b301afb5e4136cb113b69cfd20f05a1431c9
Mario Campa <mcampa@gmail.com>
2017-05-27 23:59:28 -0700

    fix merge

--- app/app.js
+++ app/app.js
@@ -22,30 +22,33 @@ module.exports = function (config, callback) {
         // 1. Check if we have the required dependencies installed
         function test_deps(next_step) {
             dependency_manager.check_deps({
-                "binaries": ["dhcpd", "hostapd", "iw"],
-                "files":    ["/etc/init.d/isc-dhcp-server"]
+                "binaries": ["dhcpcd", "hostapd", "iw", "dnsmasq"],
             }, function(error) {
                 if (error) console.log(" * Dependency error, did you run `sudo npm run-script provision`?");
                 next_step(error);
             });
         },
 
-
-        // 2. Check if ap mode is enabled / connected
-        function test_is_wifi_enabled(next_step) {
-            wifi_manager.is_ap_enabled(function(error, hw_addr) {
-                console.log('hw_addr', hw_addr);
-                if (!hw_addr) {
-                    return next_step();
-                }
-                console.log("\nAP is enabled, and hw addr is " + hw_addr);
-                wifi_manager.enable_wifi_mode({ wifi_ssid: null, wifi_passcode: null }, () => {
+        /*
+            2.
+            Check if ssid value is set inside wpa_supplicant.conf file. If the value is set, add a delay of 3 minutes
+            before testing wifi (for example in case of a power outage, modem/router might not come up as fast as the pi).
+            Do not add any delay when no wifi networks are configured (first time setup most probably).
+        */
+        function check_wpa_configuration(next_step) {
+            var wpa_conf = wifi_manager.get_wpa_conf();
+            if (wpa_conf.toLowerCase().indexOf("ssid") !== -1) {
+                console.log("Wifi ssid configured in wpa_supplicant.conf, add 3 minute delay");
+                setTimeout(function () {
                     next_step();
-                });
-            });
+                }, 1000 * 60 * 3); //3 minute delay
+            } else {
+                console.log("Wifi ssid missing from wpa_supplicant.conf, continue without delay");
+                next_step();
+            }
         },
 
-        // 2. Check if wifi is enabled / connected
+        // 3. Check if wifi is enabled / connected
         function test_is_wifi_enabled(next_step) {
             wifi_manager.is_wifi_enabled(function(error, result_ip) {
                 if (result_ip) {
@@ -58,7 +61,7 @@ module.exports = function (config, callback) {
             });
         },
 
-        // 3. Turn RPI into an access point
+        // 4. Turn RPI into an access point
         function enable_rpi_ap(next_step) {
             wifi_manager.enable_ap_mode(config.access_point.ssid, function(error) {
                 if(error) {
@@ -70,7 +73,7 @@ module.exports = function (config, callback) {
             });
         },
 
-        // 4. Host HTTP server while functioning as AP, the "api.js"
+        // 5. Host HTTP server while functioning as AP, the "api.js"
         //    file contains all the needed logic to get a basic express
         //    server up. It uses a small angular application which allows
         //    us to choose the wifi of our choosing.
